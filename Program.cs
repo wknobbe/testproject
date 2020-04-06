de7a89c622ef827b34635eeac097926c5e018501
wknobbe <wknobbe4@gmail.com>
2020-04-06 17:03:21 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
